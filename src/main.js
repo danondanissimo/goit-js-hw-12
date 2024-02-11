015ace78d205d0b552c1e0825119652d6f8e777c
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-02-11 14:05:29 +0000

    Added loadmore button functionality

--- src/main.js
+++ src/main.js
@@ -8,39 +8,50 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import axios from 'axios';
+
 const searchForm = document.querySelector('.js-search-form');
 const photoContainer = document.querySelector('.js-photo-container');
+const loadMore = document.querySelector('.js-load-more');
 const loader = document.querySelector('.loader');
+
+let currentPage = 1;
+
+let defaultPageSize = 15;
+
+let currentQuery = '';
+
+let totalResult = 0;
 
 // loader.style.display = 'none';
 
-searchForm.addEventListener('submit', e => {
-  e.preventDefault();
+// searchForm.addEventListener('submit', e => {
+//   e.preventDefault();
 
-  const name = e.target.elements.query.value;
-  loader.style.display = 'block';
-  searchPhoto(name)
-    .then(data => {
-      renderPhoto(data);
-    })
-    .finally(() => (loader.style.display = 'none'));
+//   const name = e.target.elements.query.value;
+//   loader.style.display = 'block';
+//   searchPhoto(name)
+//     .then(data => {
+//       renderPhoto(data);
+//     })
+//     .finally(() => (loader.style.display = 'none'));
 
-  e.target.reset();
-});
+//   e.target.reset();
+// });
 
-function searchPhoto(searchedImage) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const PARAMS = `?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${searchedImage}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const url = BASE_URL + PARAMS;
-  // loader.style.display = 'inline-block';
-  return fetch(url).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    } else {
-      return res.json();
-    }
-  });
-}
+// function searchPhoto(searchedImage) {
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const PARAMS = `?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${searchedImage}&image_type=photo&orientation=horizontal&safesearch=true`;
+//   const url = BASE_URL + PARAMS;
+//   // loader.style.display = 'inline-block';
+//   return fetch(url).then(res => {
+//     if (!res.ok) {
+//       throw new Error(res.status);
+//     } else {
+//       return res.json();
+//     }
+//   });
+// }
 
 function photoTemplate(photo) {
   return photo.hits
@@ -83,7 +94,7 @@ const lightbox = new SimpleLightbox('.gallery a', {
 // });
 
 function renderPhoto(photo) {
-  photoContainer.replaceChildren();
+  totalResult = photo.hits.length;
   if (photo.hits.length === 0) {
     iziToast.show({
       message:
@@ -95,7 +106,55 @@ function renderPhoto(photo) {
   } else {
     const markup = photoTemplate(photo);
     // loader.style.display = 'inline-block';
-    photoContainer.insertAdjacentHTML('afterbegin', markup);
+    photoContainer.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
+  }
+}
+
+// ====================================================================
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  photoContainer.replaceChildren();
+  currentQuery = e.target.elements.query.value;
+  loader.style.display = 'block';
+  searchPhoto(currentQuery)
+    .then(data => {
+      renderPhoto(data);
+    })
+    .finally(() => (loader.style.display = 'none'));
+
+  e.target.reset();
+  checkButtonStatus();
+});
+
+loadMore.addEventListener('click', () => {
+  currentPage += 1;
+  loader.style.display = 'block';
+  searchPhoto(currentQuery)
+    .then(data => {
+      renderPhoto(data);
+    })
+    .finally(() => (loader.style.display = 'none'));
+  checkButtonStatus();
+});
+
+function searchPhoto(searchedImage) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const PARAMS = `?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${searchedImage}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${defaultPageSize}`;
+  const url = BASE_URL + PARAMS;
+  // loader.style.display = 'inline-block';
+
+  return axios.get(url).then(res => res.data);
+}
+
+function checkButtonStatus() {
+  // const maxPage = Math.ceil(totalResult / defaultPageSize);
+  const maxPage = 2;
+  if (currentPage === maxPage) {
+    loadMore.classList.add('hidden');
+    console.log('Yes, it is');
+  } else {
+    loadMore.classList.remove('hidden');
   }
 }
